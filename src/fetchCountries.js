0e9f2eb38824cdbf7b2cec273c6faaa91927e17b
Michalkata6 <michal.kata6@gmail.com>
2023-11-19 22:50:51 +0100

    zmiany 19.11.2023

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,15 +1,17 @@
-export async function fetchCountries(name) {
-    try {
-      const response = await fetch(`https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages`);
-      
-      if (!response.ok) {
-        throw new Error('Country not found');
-      }
-      
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      throw new Error('Oops, there is no country with that name');
+async function fetchCountries(name) {
+  try {
+    const searchTerm = encodeURIComponent(name);
+    const response = await fetch(`https://restcountries.com/v3.1/name/${searchTerm}?fields=name,capital,population,flags,languages`);
+    
+    if (!response.ok) {
+      throw new Error('Failed to fetch countries');
     }
+    
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error(error.message);
   }
-  
+}
+
+export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,94 +1,55 @@
-import fetchCountries from './fetchCountries';
+import {fetchCountries} from './fetchCountries';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
-    const API_URL = `https://restcountries.com/v3.1/name/${searchTerm}?fields=name,capital,population,flags,languages`; 
+const countryList = document.getElementById("country-list")
+const countryInfo = document.getElementById("country-info")
+const searchBox = document.getElementById('search-box')
+
+  function handleSearch() {
+    const input = searchBox.value.trim();
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
   
-    const countryList = document.getElementById("country-list")
-    const countryInfo = document.getElementById("country-info")
+    if (input !== '') {
+      fetchCountries(input)
+        .then(data => {
+        console.log(data)
 
-
-    let lastSearchTerm = "";
-
-    function searchCountry() {
-      const input = document.getElementById('searchInput').value.trim();
-      if (input !== '') {
-        const searchTerm = encodeURIComponent(input); 
-     
-        fetchCountries(API_URL)
-          .then(response => {
-            if (!response.ok) {
-              throw new Error('No matching countries found.');
-            }
-            return response.json();
-          })
-          .then(data => {
-            displayCountryInfo(data);
-          })
-          .catch(error => {
-            displayError(error.message);
+        if (data.length === 0) {
+          Notiflix.Notify.failure("No matching countries found.");
+        } else if (data.length > 10) {
+          Notiflix.Notify.warning("Too many matches found. Please enter a more specific name.");
+        } else if (data.length >= 2 && data.length <= 10) 
+        {
+          data.forEach((country) => {
+            const countryItem = document.createElement("div");
+            countryItem.innerHTML = `<img src="${country.flags.svg}" alt="${country.name.official}"><p>${country.name.official}</p>`;
+            countryItem.addEventListener("click", () => showCountryDetails(country));
+            countryList.appendChild(countryItem);
           });
-      } else {
-        displayError('Too many matches found. Please enter a more specific name.');
-      }
-    }
-    
-      
-    
-    function displayCountryInfo(countryData) {
-      const countryInfoDiv = document.getElementById('countryInfo');
-      countryInfoDiv.innerHTML = '';
-      countryInfoDiv.innerHTML = countryInfo;
-    }
-    const debounce = debounce((searchTerm) => {
-      if (searchTerm.trim() === "") {
-        countryList.innerHTML = "";
-        countryDetails.innerHTML = "";
-        return;
-      }
-
-      fetchCountries(`${API_URL}?name=${searchTerm}`)
-        .then((response) => response.json())
-        .then((data) => {
-          countryList.innerHTML = "";
-          countryDetails.innerHTML = "";
-
-          if (data.length === 0) {
-            Notiflix.Notify.failure("No matching countries found.");
-          } else if (data.length > 10) {
-            Notiflix.Notify.warning("Too many matches found. Please enter a more specific name.");
-          } else if (data.length >= 2 && data.length <= 10) {
-            data.forEach((country) => {
-              const countryItem = document.createElement("div");
-              countryItem.innerHTML = `<img src="${country.flags.svg}" alt="${country.name.official}"><p>${country.name.official}</p>`;
-              countryItem.addEventListener("click", () => showCountryDetails(country));
-              countryList.appendChild(countryItem);
-            });
           } else if (data.length === 1) {
-            showCountryDetails(data[0]);
-          }
-        })
-        .catch((error) => {
-          console.error("Error fetching data:", error);
-        });
-    }, 300);
-
-    function showCountryDetails(country) {
-      countryDetails.innerHTML = `
-        <div class="country-card">
-          <img src="${country.flags.svg}" alt="${country.name.official}">
-          <h2>${country.name.official}</h2>
-          <p><strong>Capital:</strong> ${country.capital}</p>
-          <p><strong>Population:</strong> ${country.population}</p>
-          <p><strong>Languages:</strong> ${Object.values(country.languages).join(', ')}</p>
-        </div>
-      `;
-    }
-
-    searchBox.addEventListener("input", (event) => {
-      const searchTerm = event.target.value.trim();
-      if (searchTerm !== lastSearchTerm) {
-        lastSearchTerm = searchTerm;
-        debounce(searchTerm);
+          showCountryDetails(data[0]);
       }
-    });
+        })
+        .catch(error => {
+          
+          console.error(error);
+        });
+    } 
+    }
+  
+  searchBox.addEventListener('input', debounce(handleSearch, 300));
+
+          
+  function showCountryDetails(country) {
+    countryInfo.innerHTML = `
+      <div class="country-card">
+        <img src="${country.flags.svg}" alt="${country.name.official}">
+        <h2>${country.name.official}</h2>
+        <p><strong>Capital:</strong> ${country.capital}</p>
+        <p><strong>Population:</strong> ${country.population}</p>
+        <p><strong>Languages:</strong> ${Object.values(country.languages).join(', ')}</p>
+      </div>
+    `;
+  }
